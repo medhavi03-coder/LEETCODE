8eb461e3c0ac0ea423280b9b74590199b9e14863
Medhavi Chitranshi <medhavi.24mca10165@vitbhopal.ac.in>
2025-05-20 21:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (66.44%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long val = 1;
+        row.add(1); // C(rowIndex, 0) = 1
+
+        for (int i = 1; i <= rowIndex; i++) {
+            val = val * (rowIndex - i + 1) / i;
+            row.add((int) val);
+        }
+
+        return row;
+    }
+}
