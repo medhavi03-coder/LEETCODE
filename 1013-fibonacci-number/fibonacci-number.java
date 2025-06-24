0b8bbadd5c186665d7ada26567b88425988e68a1
Medhavi Chitranshi <medhavi.24mca10165@vitbhopal.ac.in>
2025-06-24 18:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (28.14%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -7,10 +7,22 @@ class Solution {
 
     private int fibMemo(int n, int[] dp) {
         if (n <= 1) return n;
-
-        if (dp[n] != -1) return dp[n];
-
-        dp[n] = fibMemo(n - 1, dp) + fibMemo(n - 2, dp);
-        return dp[n];
+        int current=0, prev=1,prev2=0;
+        for(int i=2;i<=n;i++){
+            current=prev+prev2;
+            prev2=prev;
+            prev=current;
+        }
+        return current;
+// 
+        // dp[0]=0;
+        // dp[1]=1;
+        // for(int i=2; i<=n;i++){
+        //     dp[i]= dp[i-1]+ dp[i-2];
+        // }
+        // if (dp[n] != -1) return dp[n];
+// 
+        // dp[n] = fibMemo(n - 1, dp) + fibMemo(n - 2, dp);
+        // return dp[n];
     }
 }
